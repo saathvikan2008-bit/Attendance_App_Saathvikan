1cb8e810c2495bb82c888079aa7828de9f60bcf0
Saathvikan Mohankumar <saathvikan2008@gmail.com>
2025-09-09 21:11:14 +0530

    A seperate function for face recogntion is created

--- Main.py
+++ Main.py
@@ -45,6 +45,47 @@ def tempimgdel(path):
     elif os.path.exists(path) == False:
         print("Temp file does not Exist, Quitting")
 
+#A Function to perform the Normal Face recognition Operation
+def Face_recognition():
+    # Perform face recognition
+    result = DeepFace.find(img_path=temp_img_path, db_path=cwd+"/RegisteredFaces", enforce_detection=False)
+
+    # Print the closest match's name if found
+    if len(result)>0:
+        df = result[0] #Takes the closest match
+
+    # If a face is matched
+    if not df.empty:
+
+        # Face Recognition
+        matched_path = df.iloc[0]["identity"]
+        folder_path = os.path.dirname(matched_path)
+        person_ID = os.path.basename(folder_path)
+        print("Recognised Person:", person_ID)
+        formatted_time = (datetime.datetime.now()).strftime("%H:%M:%S")
+        
+        #Updates the CSV file
+        updation_Success = update_record(person_ID, formatted_time)
+        cv2.putText(frame_flipped, updation_Success, (0, frame_flipped.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        print(updation_Success)
+
+        #Drawing a box and displaying the name of the person
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+        gray = cv2.cvtColor(frame_flipped, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        for (x,y,w,h) in faces:
+            #Draw a rectangle around the persons face
+            cv2.rectangle(frame_flipped, (x,y), (x+w, y+h), (0,0,255), 2)
+
+            #Display the name of the person detected above the rectangle
+            cv2.putText(frame_flipped, person_ID, (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
+            break
+
+    else:
+        print("Standby")
+        cv2.putText(frame_flipped, "Waiting for a Face", (0, 35), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0,0), 2)
+
 initcheck() 
 
 cap = cv2.VideoCapture(0)
@@ -63,45 +104,8 @@ while True:
     temp_img_path = "temp.jpg"
     cv2.imwrite(temp_img_path, frame_flipped)
 
-    # Perform face recognition
-    result = DeepFace.find(img_path=temp_img_path, db_path=cwd+"/RegisteredFaces", enforce_detection=False)
-    
-    # Print the closest match's name if found
-    if len(result)>0:
-        df = result[0] #Takes the closest match
+    Face_recognition() #Calls the face recognition
 
-        # If a face is matched
-        if not df.empty:
-
-            # Face Recognition
-            matched_path = df.iloc[0]["identity"]
-            folder_path = os.path.dirname(matched_path)
-            person_ID = os.path.basename(folder_path)
-            print("Recognised Person:", person_ID)
-            formatted_time = (datetime.datetime.now()).strftime("%H:%M:%S")
-            
-            #Updates the CSV file
-            updation_Success = update_record(person_ID, formatted_time)
-            cv2.putText(frame_flipped, updation_Success, (0, frame_flipped.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
-            print(updation_Success)
-
-            #Drawing a box and displaying the name of the person
-            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
-            gray = cv2.cvtColor(frame_flipped, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-            for (x,y,w,h) in faces:
-                #Draw a rectangle around the persons face
-                cv2.rectangle(frame_flipped, (x,y), (x+w, y+h), (0,0,255), 2)
-
-                #Display the name of the person detected above the rectangle
-                cv2.putText(frame_flipped, person_ID, (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
-                break
-
-        else:
-            print("Standby")
-            cv2.putText(frame_flipped, "Waiting for a Face", (0, 35), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0,0), 2)
-    
     # displays q to quit
     cv2.putText(frame_flipped, "'q' to quit", (frame_flipped.shape[1]-125,25), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,0), 1)
 
@@ -112,7 +116,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         tempimgdel(temp_img_path)
         break
-    
+
 #Close the Webcam and destroy all windows
 cap.release()
 cv2.destroyAllWindows()
