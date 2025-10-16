46e41e6001aadfdede72b439193d50a5992fa7e2
Saathvikan Mohankumar <saathvikan2008@gmail.com>
2025-10-16 23:05:05 +0530

    Added Tkinter support

---
+++ scratch.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Define two matrices
+matrix1 = np.array([[1, 0], [0, 1]])
+matrix2 = np.array([[5, 6], [7, 8]])
+
+# Perform matrix multiplication
+result = np.dot(matrix1, matrix2)
+r = np.cross
+
+# Display the result
+print("Matrix 1:")
+print(matrix1)
+print("\nMatrix 2:")
+print(matrix2)
+print("\nResult of Matrix Multiplication:")
+print(result)
